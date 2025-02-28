6362f0fcfaeda39ef24dade57fbc52867224352f
runkle-austin <166458209+runkle-austin@users.noreply.github.com>
2025-02-27 21:34:51 -0700

    Add files via upload
    added a decent amount of stuff mroe so in Library model than in music store but i think im done for tonight

--- model/LibraryModel.java
+++ model/LibraryModel.java
@@ -2,20 +2,19 @@ package model;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class LibraryModel {
 	private MusicStore musicStore;
 	private ArrayList<Song> songs;
 	private ArrayList<Album> albums;
-	private HashMap<String, Playlist> playlists;
+	private ArrayList<Playlist> playlists;
 	private ArrayList<Song> favoriteSongs;
 
 	public LibraryModel() throws FileNotFoundException {
 		musicStore = new MusicStore();
 		songs = new ArrayList<>();
 		albums = new ArrayList<>();
-		playlists = new HashMap<>(); 
+		playlists = new ArrayList<>(); 
 		favoriteSongs = new ArrayList<>();
 	}
 	// finds song from songName and artistName in music store and creates copy
@@ -46,40 +45,150 @@ public class LibraryModel {
 		return songName + " was added to Library";
 	}
 
+	// this method gets all of the songs in the library by artist
 	public String getSongsByArtist(String artist) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	public String getAlbumsByTitle(String title) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	public String getAlbumsByArtist(String artist) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	public String getPlaylistByName(String name) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	public void createPlaylist(String name) {
-		// TODO Auto-generated method stub
+		String result = "Songs by " + artist + "\n";
 		
-	}
-	public void addSongToPlaylist(String playlistName, String songTitle) {
-		// TODO Auto-generated method stub
+		// loop through all the songs in the library
+		for (Song song: songs) {
+			if (song.getArtist().equals(artist));
+			result += song.toString();
+		}
 		
+		if (result.equals("")) {
+			return "No songs by " + artist; 
+		} else {
+			return result;
+		}
 	}
-	public void addAlbumToPlaylist(String playlistName, String albumTitle) {
-		// TODO Auto-generated method stub
-		
-	}
-	public void removeSongFromPlaylist(String playlistName, String songTitle) {
-		// TODO Auto-generated method stub
-		
-	}
+	
+	// this method gets all of the songs in the library by title
 	public String getSongsByTitle(String title) {
-		// TODO Auto-generated method stub
-		return null;
+		String result = "Songs named" + title + "\n";
+		
+		// loop through all the songs in the library
+		for (Song song: songs) {
+			if (song.getTitle().equals(title));
+			result += song.toString();
+		}
+		
+		if (result.equals("")) {
+			return "No songs with title " + title; 
+		} else {
+			return result;
+		}
 	}
+	
+	public String getAlbumsByTitle(String title) {
+		String result = "Albums named" + title + "\n";
+		
+		// loop through all the albums in the library
+		for (Album album: albums) {
+			if (album.getTitle().equals(title));
+			result += album.toString();
+		}
+		
+		if (result.equals("")) {
+			return "No albums with title " + title; 
+		} else {
+			return result;
+		}
+	}
+	
+	public String getAlbumsByArtist(String artist) {
+		String result = "Albums by " + artist + "\n";
+		
+		// loop through all the albums in the library
+		for (Album album: albums) {
+			if (album.getArtist().equals(artist));
+			result += album.toString();
+		}
+		
+		if (result.equals("")) {
+			return "No songs with title " + artist; 
+		} else {
+			return result;
+		}
+	}
+	
+	// this method creates a new playlist and adds it to the arraylist of playlists
+	public void createPlaylist(String name) {
+		playlists.add(new Playlist(name));
+	}
+	
+	// this method adds a song to a specific playlist in the playlist arraylist
+	public String addSongToPlaylist(String playlistName, String songTitle, String artist) {
+		for (Playlist p: playlists) {
+			if (p.getName().equals(playlistName)) {
+				Song song = createSong(songTitle, artist);
+				if (song.equals(null)) {
+					return "Song was not found in music store";
+				}
+				p.addSong(song);
+				return songTitle + " has been added to playlist" + playlistName + "\n";
+			}
+		}
+		// if the playlist does not exist
+		return playlistName + " has not been created " + "\n"; 
+		
+	}
+	
+	public String addAlbumToPlaylist(String playlistName, String albumTitle, String artist) {
+		for (Playlist p: playlists) {
+			if (p.getName().equals(playlistName)) {
+				Album album = musicStore.getAlbumByTitleAndArtist(albumTitle, artist);
+				if (album.equals(null)) {
+					return "Album was not found in music store";
+				}
+				p.addAlbum(albumTitle);
+				return albumTitle + " has been added to playlist" + playlistName + "\n";
+			}
+		}
+		// if the playlist does not exist
+		return playlistName + " has not been created " + "\n"; 
+	}
+	
+	public String getPlaylistByName(String name) {
+		for (Playlist p : playlists) {
+			if (p.getName().equals(name)) {
+				return p.toString();
+			}
+		}
+		// if the playlist does not exist
+		return name + " has not been created " + "\n"; 
+	}
+	
+	public String removeSongFromPlaylist(String playlistName, String songTitle) {
+		// loop through the playlists
+		for (Playlist p: playlists) {
+			// if the playlist is found remove the song from the playlist
+			if (p.getName().equals(playlistName)) {
+				p.removeSong(songTitle);
+				return songTitle + " has been removed from playlist" + playlistName;
+			}
+		}
+		
+		// if the playlist does not exist
+		return playlistName + " does nto exist " + "\n"; 
+	}
+	
+	public String rateSong(String song, String artist, int rating) {
+		// find the song
+		for (Song s: songs) {
+			if (s.getTitle().equals(song) && s.getArtist().equals(artist)) {
+				//rate the song and tell the user it has been rated
+				s.setRating(rating);
+				//add the song to the list of favorite songs
+				if (rating == 5) {
+					favoriteSongs.add(s);
+				}
+				return song + "has been rated\n";
+			}
+		}
+		
+		// if the song was not found dont rate it
+		return "Song was not found";
+	}
+	
+
 }
